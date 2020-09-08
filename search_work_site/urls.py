d7751ee82f27f8592f4272e131ad6ed67113c8c5
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-09-09 00:19:34 +0300

    Add error pages

--- search_work_site/urls.py
+++ search_work_site/urls.py
@@ -1,6 +1,7 @@
+from os import name
 from django.urls import path
 from .views import home_view
 
 urlpatterns = [
-    path('', home_view, name='home_page')
+    path('', home_view, name='home_page'),
 ]
